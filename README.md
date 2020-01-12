907d745c88a659f1114230181b666bb6d73dc2f0
zuizihad <ahmedxihad@gmail.com>
2020-01-12 17:10:37 +0600

    webpack enrty output & mode concepts

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # js-config
 
+webpack basic concepts:
+1. Entry
+2. Output
+3. Loaders
+4. Plugins
+5. Mode
 
 npm init -y
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
 module.exports = {
+    //webpack entry pint
     entry: './src/scripts/index.js',
+    //webpack output point
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    //webpack mode
     mode: 'development'  
 }
